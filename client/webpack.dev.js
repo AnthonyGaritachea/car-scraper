430973d0509cc269c3e5704fa0e8bebcc0cf17ae
anthonygaritachea <lgaritachea45204@gmail.com>
2021-09-15 18:38:00 -0700

    refactors folder structure and adds react frontend

---
+++ client/webpack.dev.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'public')
+    },
+    module: {
+        rules: [
+            {
+                loader: 'babel-loader',
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/
+            },
+            {
+                use: ['style-loader', 'css-loader'],
+                test: /\.css$/,
+            }
+        ]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.join(__dirname, 'public'),
+        // proxy: {
+        //     '/scraper': 'http://localhost:5000',
+        //     '/stories': 'http://localhost:5000'
+        //   }
+    }
+}
